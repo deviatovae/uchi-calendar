2889afa487bfb85f42610cb139d3e5d96018ca54
Alena Deviatova <aedeviatova@yandex.ru>
2023-05-21 22:50:21 +0300

    add theme and global styles

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
+import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 
@@ -8,7 +9,10 @@ const GlobalStyles = createGlobalStyle`
   ${normalize}
   body {
     height: 100vh;
+    width: 100vw;
     font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
   }
   * {
     box-sizing: border-box;
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    textMain: '#000',
+    textSecondary: '#555e66',
+    bgMain: '#fff',
+    bgSecondary: '#dadada',
+    bgCells: '#ced1f5',
+    highlight: '#b61f13',
+    highlightHover: '#e86b66',
+
+  },
+  contentMaxWidth: '740px',
+  contentMinWidth: '320px',
+  headerHeight: '70px',
+};
+
+export default theme;
